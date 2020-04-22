121e834c32f417fd456a1e2813d2321fa4a112ae
Justin Spegele <justin.spegele@gmail.com>
2020-04-22 12:30:55 -0400

    Removed conditional rendering from Expense Summary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -9,12 +9,9 @@ export const ExpensesSummary = ({ expenseCount, expensesTotal}) => {
   const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00')
   return (
     <div>
-      {
-        expenseCount > 0 &&
-        <p>
-          Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal}.
-        </p>
-      }
+      <p>
+        Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal}.
+      </p>
     </div>
   )
 };
